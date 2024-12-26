98d7d17c73195aa3e5025f98e446ac6660acef90
Jean-Marc Gulliet <jeanmarc.gulliet@gmail.com>
2024-12-26 18:59:56 +0100

    fix: path to movie picture

--- js/templates/MovieCard.js
+++ js/templates/MovieCard.js
@@ -1,17 +1,17 @@
 class MovieCard {
     constructor(movie) {
-        this._movie = movie
+        this._movie = movie;
     }
 
     createMovieCard() {
-        const $wrapper = document.createElement('div')
-        $wrapper.classList.add('movie-card-wrapper')
+        const $wrapper = document.createElement("div");
+        $wrapper.classList.add("movie-card-wrapper");
 
         const movieCard = `
             <div class="movie-thumbnail center">
                 <img
                     alt="${this._movie.title}"
-                    src="/assets/thumbnails/${this._movie.picture}"
+                    src="${this._movie.picture}"
                 />
             </div>
             <h3 class="fs-16 center">${this._movie.title}</h3>
@@ -20,9 +20,9 @@ class MovieCard {
                 -
                 <span>${this._movie.duration}</span>
             </p>
-        `
-        
-        $wrapper.innerHTML = movieCard
-        return $wrapper
+        `;
+
+        $wrapper.innerHTML = movieCard;
+        return $wrapper;
     }
 }
